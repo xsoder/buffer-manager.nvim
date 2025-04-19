95f7f4b3331b42ac77699a6e0801b077ae97344d
csodelinux <csodelinux@gmail.com>
2025-04-19 15:42:49 -0400

    feat : auto formnat

--- test/buffer_manager_spec.lua
+++ test/buffer_manager_spec.lua
@@ -5,35 +5,35 @@ package.path = package.path .. ";./lua/?.lua;./lua/?/init.lua"
 
 -- Minimal vim mock for busted
 if not _G.vim then
-	_G.vim = {
-		g = {},
-		fn = setmetatable({}, {
-			__index = function(_, key)
-				return function()
-					return nil
-				end
-			end,
-		}),
-		api = setmetatable({}, {
-			__index = function(_, key)
-				return function()
-					return nil
-				end
-			end,
-		}),
-	}
+    _G.vim = {
+        g = {},
+        fn = setmetatable({}, {
+            __index = function(_, key)
+                return function()
+                    return nil
+                end
+            end,
+        }),
+        api = setmetatable({}, {
+            __index = function(_, key)
+                return function()
+                    return nil
+                end
+            end,
+        }),
+    }
 end
 
 local buffer_manager = require("buffer-manager")
 
 describe("buffer-manager", function()
-	it("should load the module", function()
-		assert.is_table(buffer_manager)
-	end)
+    it("should load the module", function()
+        assert.is_table(buffer_manager)
+    end)
 
-	it("should have a setup function", function()
-		assert.is_function(buffer_manager.setup)
-	end)
+    it("should have a setup function", function()
+        assert.is_function(buffer_manager.setup)
+    end)
 
-	-- Add more tests as the plugin evolves
+    -- Add more tests as the plugin evolves
 end)
